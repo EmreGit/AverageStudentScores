cd585ed68c92c01cd51a9e044c542f1106e9bda0
Emre Elagoz <emreelagoz1994@gmail.com>
2020-10-23 12:06:04 +0200

    Add project files.

---
+++ AverageStudentScores/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageStudentScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter then number of students: ");
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            double averageGrade = 0;
+            double highestGrade = 0;
+            string highestGradeName = ""; //string.Empty(); is also possible
+
+            string[] studentNames = new string[numberOfStudents];
+            double[] grades = new double[numberOfStudents];
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                Console.WriteLine($"Enter the name of student number {i+1} name");
+                string name = Console.ReadLine();
+                studentNames[i] = name;
+                Console.WriteLine($"Enter the grade for student number {i+1}");
+                double grade = double.Parse(Console.ReadLine());
+                grades[i] = grade;
+                averageGrade += grade;
+                if (grade > highestGrade)
+                {
+                    highestGrade = grade;
+                    highestGradeName = name;
+                }
+
+            }
+
+            averageGrade = averageGrade / numberOfStudents;
+            Console.WriteLine($"The average grade of the class is {averageGrade}");
+            Console.WriteLine($"The highest grade of the class is {highestGrade}");
+            Console.WriteLine($"The highest grade of the class is {highestGradeName}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FindBiggestNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindBiggestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = {5, 10, 8, 2, 6, 25, 60, 10, 16, 28 };
+
+            int biggestNr = 0;
+            int index = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > biggestNr)
+                {
+                    biggestNr = array[i];
+                    index = Array.IndexOf(array, array[i]);
+                }
+            }
+            Console.WriteLine($"The biggest number is {biggestNr} {index}");
+        }
+    }
+}
